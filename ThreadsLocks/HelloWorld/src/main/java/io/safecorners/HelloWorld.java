8b28d1adc095fb60b685a4018bedbcde4994970e
alifeunfolds <alifeunfolds@naver.com>
2016-10-28 11:27:15 +0900

    add counting.java

---
+++ ThreadsLocks/HelloWorld/src/main/java/io/safecorners/HelloWorld.java
@@ -0,0 +1,17 @@
+package io.safecorners;
+
+// START:main
+public class HelloWorld {
+    public static void main( String[] args ) throws InterruptedException {
+      Thread myThread = new Thread() {
+        public void run() {
+          System.out.println("Hello from new thread");
+        }
+      };
+    
+    myThread.start();
+    Thread.yield();
+    System.out.println("Hello from main thread");
+    myThread.join();
+    }
+}
